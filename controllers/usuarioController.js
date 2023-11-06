b9510e8ddda06204b5673e67026af8e76ecd4c30
emartinez83 <emartinez83@uabc.edu.mx>
2023-11-06 02:08:47 -0800

    Ya todo funciona bien por el momento revisa en la base de datos y conforme las credenciales sean correctas procede a iniciar sesion y mandarlo a la vista de la donacion

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,32 +2,39 @@ const Usuario = require('../models/usuarios');
 
 // Controlador para procesar el inicio de sesión
 exports.iniciarSesion = (req, res) => {
-    const { correo, contrasena } = req.body;
-    console.log('datos',req.body);
+    const { correo_electronico, contrasena } = req.body;
+    console.log('datos', req.body);
 
     // Obtener todos los usuarios
     Usuario.obtenerTodosLosUsuarios((error, usuarios) => {
+        console.log('bien');
         if (error) {
             // Manejo de errores
+            console.error(error);
             res.status(500).send('Error en el servidor');
+            return; // Salir del controlador
         }
 
         // Buscar un usuario con el correo proporcionado
-        const usuario = usuarios.find((u) => u.correo === correo);
+        const usuario = usuarios.find((u) => u.correo_electronico === correo_electronico);
+        console.log('bien2')
 
         if (!usuario) {
+            console.log('mal');
             // Usuario no encontrado, muestra un mensaje de error
             res.render('login.twig', { error: 'Usuario no encontrado' });
         } else {
+            console.log('bienx3');
             // Usuario encontrado, verifica la contraseña
             if (contrasena === usuario.contrasena) {
+                console.log('vamo');
                 // Contraseña correcta, redirige al usuario a la página deseada
                 res.redirect('/donacion');
             } else {
+                console.log('malx2');
                 // Contraseña incorrecta, muestra un mensaje de error
                 res.render('login.twig', { error: 'Credenciales incorrectas' });
             }
         }
     });
 };
-
--- models/usuarios.js
+++ models/usuarios.js
@@ -2,19 +2,19 @@ const db = require('../config/database'); // Reemplaza con la ruta correcta a tu
 
 // Modelo de usuario
 class Usuario {
-    constructor(correo, contrasena) {
-        this.correo = correo;
+    constructor(correo_electronico, contrasena) {
+        this.correo_electronico = correo_electronico;
         this.contrasena = contrasena;
     }
 
     static obtenerTodosLosUsuarios(callback) {
-        const consulta = 'SELECT * FROM usuario';
+        const consulta = 'SELECT correo_electronico, contrasena FROM usuarios';
         db.query(consulta, (error, resultados) => {
             if (error) {
                 return callback(error, null);
             }
             const usuarios = resultados.map((resultado) => {
-                return new Usuario(resultado.correo, resultado.contrasena);
+                return new Usuario(resultado.correo_electronico, resultado.contrasena);
             });
             callback(null, usuarios);
         });
